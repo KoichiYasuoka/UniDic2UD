683069618ded75ab8d7c41f2fac83f94bd73a36e
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-09-23 14:54:21 +0900

    bug fix

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 import setuptools
+import os
 with open("README.md","r") as r:
   long_description=r.read()
 URL="https://github.com/KoichiYasuoka/UniDic2UD"
+if os.uname().sysname.startswith("CYGWIN"):
+  if not os.path.isfile("/usr/local/bin/mecab-config"):
+    import pip._internal
+    if os.uname().machine=="x86_64":
+      repository="https://github.com/KoichiYasuoka/mecab-cygwin64"
+    else:
+      repository="https://github.com/KoichiYasuoka/mecab-cygwin32"
+    pip._internal.main(["install","git+"+repository])
 
 setuptools.setup(
   name="unidic2ud",
-  version="1.1.9",
+  version="1.2.1",
   description="Tokenizer POS-tagger Lemmatizer and Dependency-parser for modern and contemporary Japanese",
   long_description=long_description,
   long_description_content_type="text/markdown",
