cbcc033622ef2ee32fcb276f5bcf613da29affe1
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-11-12 11:11:56 +0900

    Tree.renew() supported

--- unidic2ud/cabocha/unidic2cabocha.py
+++ unidic2ud/cabocha/unidic2cabocha.py
@@ -49,8 +49,8 @@ class Tree(unidic2ud.UDPipeEntry):
       s.append(c)
     if s!=[]:
       p.append(s)
-    self._sentences=p
-    self._chunkinfo=[]
+    self._cabocha._sentences=p
+    self._cabocha._chunkinfo=[]
     k=0
     for s in p:
       ix=[-1]
@@ -79,9 +79,9 @@ class Tree(unidic2ud.UDPipeEntry):
             w+=" "
         cf.append((i,z,c[0]-1,y,j,w,k))
         k+=1
-      self._chunkinfo.append(cf)
+      self._cabocha._chunkinfo.append(cf)
   def _makeFeatures(self):
-    self._features=[""]
+    self._cabocha._features=[""]
     for t in range(1,len(self)):
       x=(self[t].xpos+"-*-*-*-*-*-*-*-*-*").split("-")
       x[6]=self[t].lemma
@@ -89,20 +89,20 @@ class Tree(unidic2ud.UDPipeEntry):
       if j>=0:
         x[7]=self[t].misc[j+9:]
       x[9]=self[t].upos
-      self._features.append(",".join(x[0:10]))
+      self._cabocha._features.append(",".join(x[0:10]))
   def toString(self,format=4):
     if format==4:
       return str(self)
-    if not hasattr(self,"_sentences"):
+    if not hasattr(self._cabocha,"_sentences"):
       self._makeChunks()
-    if format>0 and not hasattr(self,"_features"):
+    if format>0 and not hasattr(self._cabocha,"_features"):
       self._makeFeatures()
     result=""
-    for k,s in enumerate(self._sentences):
+    for k,s in enumerate(self._cabocha._sentences):
       if format==0 or format==2:
-        x=len(self._chunkinfo[k])
+        x=len(self._cabocha._chunkinfo[k])
         l,m,n=[],[],[" "*(x*2)]*x
-        for i,d,t,h,f,w,z in self._chunkinfo[k]:
+        for i,d,t,h,f,w,z in self._cabocha._chunkinfo[k]:
           l.append(len(w)+len([c for c in w if ord(c)>127]))
           m.append(w)
           if d<0:
@@ -115,10 +115,10 @@ class Tree(unidic2ud.UDPipeEntry):
           result+=" "*(h-(x-i)*2-l[i])+m[i]+n[i][i*2:].rstrip()+"\n"
         result+="EOS\n"
       if format==1 or format==2:
-        for i,d,t,h,f,w,z in self._chunkinfo[k]:
+        for i,d,t,h,f,w,z in self._cabocha._chunkinfo[k]:
           result+="* "+str(i)+" "+str(d)+"D "+str(h)+"/"+str(f)+" 0.000000\n"
           for t in s[i]:
-            result+=self[t].form+"\t"+self._features[t]+"\t"+str(self[t].id)+"<-"+self[t].deprel
+            result+=self[t].form+"\t"+self._cabocha._features[t]+"\t"+str(self[t].id)+"<-"+self[t].deprel
             if self[t] is self[t].head:
               result+="\n"
             else:
@@ -126,10 +126,10 @@ class Tree(unidic2ud.UDPipeEntry):
         result+="EOS\n"
       if format==3:
         result+="<sentence>\n"
-        for i,d,t,h,f,w,z in self._chunkinfo[k]:
+        for i,d,t,h,f,w,z in self._cabocha._chunkinfo[k]:
           result+=' <chunk id="'+str(i)+'" link="'+str(d)+'" rel="D" score="0.000000" head="'+str(self[s[i][h]].id)+'" func="'+str(self[s[i][f]].id)+'">\n'
           for t in s[i]:
-            result+='  <tok id="'+str(self[t].id)+'" feature="'+self._features[t]+'" head="'+str(self[t].head.id)+'" rel="'+self[t].deprel+'">'+self[t].form+'</tok>\n'
+            result+='  <tok id="'+str(self[t].id)+'" feature="'+self._cabocha._features[t]+'" head="'+str(self[t].head.id)+'" rel="'+self[t].deprel+'">'+self[t].form+'</tok>\n'
           result+=" </chunk>\n"
         result+="</sentence>\n"
     return result
@@ -138,10 +138,10 @@ class Tree(unidic2ud.UDPipeEntry):
   def token_size(self):
     return len(self)-1
   def token(self,index):
-    if not hasattr(self,"_sentences"):
+    if not hasattr(self._cabocha,"_sentences"):
       self._makeChunks()
     c=None
-    for s1,s2 in zip(self._sentences,self._chunkinfo):
+    for s1,s2 in zip(self._cabocha._sentences,self._cabocha._chunkinfo):
       for c1,c2 in zip(s1,s2):
         i,d,t,h,f,w,z=c2
         if t==index:
@@ -149,25 +149,31 @@ class Tree(unidic2ud.UDPipeEntry):
           break
     return Token(self,c,index)
   def chunk_size(self):
-    if not hasattr(self,"_sentences"):
+    if not hasattr(self._cabocha,"_sentences"):
       self._makeChunks()
-    return sum(len(s) for s in self._sentences)
+    return sum(len(s) for s in self._cabocha._sentences)
   def chunk(self,index):
-    if not hasattr(self,"_sentences"):
+    if not hasattr(self._cabocha,"_sentences"):
       self._makeChunks()
-    for s1,s2 in zip(self._sentences,self._chunkinfo):
+    for s1,s2 in zip(self._cabocha._sentences,self._cabocha._chunkinfo):
       for c1,c2 in zip(s1,s2):
         i,d,t,h,f,w,z=c2
         if z==index:
           return Chunk(c1,c2)
     return None
   def sentence_size(self):
-    if not hasattr(self,"_sentences"):
+    if not hasattr(self._cabocha,"_sentences"):
       self._makeChunks()
-    return len(self._sentences)
+    return len(self._cabocha._sentences)
   def sentence(self,index=0):
-    p=self._result.split("# text = ")
-    return p[index+1].split("\n")[0]
+    if self._result>"":
+      return self._result.split("# text = ")[index+1].split("\n")[0]
+    if not hasattr(self._cabocha,"_sentences"):
+      self._makeChunks()
+    return "".join(w for i,d,t,h,f,w,z in self._cabocha._chunkinfo[index])
+  def renew(self):
+    self._makeChunks()
+    self._makeFeatures()
 
 class Chunk(object):
   additional_info=None
@@ -191,9 +197,9 @@ class Token(object):
   def __init__(self,tree,chunk,index):
     self.surface=self.normalized_surface=tree[index+1].form
     self.chunk=chunk
-    if not hasattr(tree,"_features"):
+    if not hasattr(tree._cabocha,"_features"):
       tree._makeFeatures()
-    self.feature=tree._features[index+1]
+    self.feature=tree._cabocha._features[index+1]
     self.feature_list_size=len(self.feature.split(","))
   def __repr__(self):
     return self.normalized_surface
@@ -201,15 +207,16 @@ class Token(object):
     return self.feature.split(",")[index]
 
 class UniDic2CaboCha(object):
-  def __init__(self,UniDic):
+  def __init__(self,parser):
+    self._parser=parser
+
+class Parser(object):
+  def __init__(self,UniDic="ipadic"):
     self.UniDic2UD=unidic2ud.UniDic2UD(UniDic,"japanese-gsd")
   def parse(self,sentence):
-    return Tree(self.UniDic2UD(sentence,raw=True))
+    t=Tree(self.UniDic2UD(sentence,raw=True))
+    t._cabocha=UniDic2CaboCha(self)
+    return t
   def parseToString(self,sentence):
     return self.parse(sentence).toString(0)
-
-def Parser(UniDic=None):
-  if UniDic==None:
-    UniDic="ipadic"
-  return UniDic2CaboCha(UniDic)
 
