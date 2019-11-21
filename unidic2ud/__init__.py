fa7a2a6524b0c1002cc624e412a0300283b441fc
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-11-21 20:28:25 +0900

    version 1.4.6

--- unidic2ud/__init__.py
+++ unidic2ud/__init__.py
@@ -1,3 +1,3 @@
-from .unidic2ud import UDPipeEntry,UniDic2UD,load,download,dictlist
+from .unidic2ud import UniDic2UDEntry,UDPipeEntry,UniDic2UD,load,download,dictlist
 
-__all__=[ UDPipeEntry,UniDic2UD,load,download,dictlist ]
+__all__=[ UniDic2UDEntry,UDPipeEntry,UniDic2UD,load,download,dictlist ]
