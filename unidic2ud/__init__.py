0182135416944e53ea9a652083cce89283a496db
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-11-22 17:26:27 +0900

    version 1.4.9

--- unidic2ud/__init__.py
+++ unidic2ud/__init__.py
@@ -1,3 +1,3 @@
-from .unidic2ud import UniDic2UDEntry,UDPipeEntry,UniDic2UD,load,download,dictlist
+from .unidic2ud import UniDic2UDEntry,UDPipeEntry,UniDic2UD,load,download,dictlist,DOWNLOAD_DIR
 
-__all__=[ UniDic2UDEntry,UDPipeEntry,UniDic2UD,load,download,dictlist ]
+__all__=[ UniDic2UDEntry,UDPipeEntry,UniDic2UD,load,download,dictlist,DOWNLOAD_DIR ]
