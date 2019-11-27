dce0a77342503ebee6ea853225c606b969dc9067
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-11-27 10:38:01 +0900

    version 1.5.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ URL="https://github.com/KoichiYasuoka/UniDic2UD"
 
 setuptools.setup(
   name="unidic2ud",
-  version="1.5.1",
+  version="1.5.2",
   description="Tokenizer POS-tagger Lemmatizer and Dependency-parser for modern and contemporary Japanese",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -21,8 +21,10 @@ setuptools.setup(
     "unidic2ud":["./*.js"],
   },
   entry_points={
-    "console_scripts":["unidic2ud=unidic2ud.cli:main"],
-    "console_scripts":["udcabocha=unidic2ud.cabocha.cli:main"],
+    "console_scripts":[
+      "unidic2ud=unidic2ud.cli:main",
+      "udcabocha=unidic2ud.cabocha.cli:main",
+    ],
   },
   classifiers=[
     "License :: OSI Approved :: MIT License",
