b65fcbd8703859e6c53a574fe06558c7a4c0b371
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-01-10 11:53:22 +0900

    version 1.7.9

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ else:
 
 setuptools.setup(
   name="unidic2ud",
-  version="1.7.8",
+  version="1.7.9",
   description="Tokenizer POS-tagger Lemmatizer and Dependency-parser for modern and contemporary Japanese",
   long_description=long_description,
   long_description_content_type="text/markdown",
