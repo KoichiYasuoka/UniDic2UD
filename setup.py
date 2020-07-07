4c8adc055f782419cddc67fe54ab183289f5b93a
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-07-07 22:11:18 +0900

    version 2.2.6

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ else:
 
 setuptools.setup(
   name="unidic2ud",
-  version="2.2.5",
+  version="2.2.6",
   description="Tokenizer POS-tagger Lemmatizer and Dependency-parser for modern and contemporary Japanese",
   long_description=long_description,
   long_description_content_type="text/markdown",
@@ -38,7 +38,7 @@ setuptools.setup(
   install_requires=install_requires,
   python_requires=">=3.6",
   package_data={
-    "unidic2ud":["./*.js","./download/*.udpipe"],
+    "unidic2ud":["./mecabrc","./*.js","./download/*.udpipe"],
   },
   entry_points={
     "console_scripts":[
