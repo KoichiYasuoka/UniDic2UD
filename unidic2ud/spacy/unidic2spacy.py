c655698cdef04d71335fedd6aa4a0cdb293a0240
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-07-28 18:20:48 +0900

    MeCab2Sudachi

--- unidic2ud/spacy/unidic2spacy.py
+++ unidic2ud/spacy/unidic2spacy.py
@@ -75,10 +75,76 @@ class UniDicTokenizer(object):
     doc.is_parsed=True
     return doc
 
+class MeCab2Sudachi(object):
+  def __init__(self,UniDic):
+    import unidic2ud
+    self.tagger=unidic2ud.UniDic2UD(UniDic,None)
+    self.UniDic=UniDic
+  def __call__(self,text):
+    r=[]
+    for t in text.split("\n"):
+      for s in self.tagger.mecab(t).split("\n"):
+        if s=="" or s=="EOS":
+          continue
+        if s.find("\t")>0:
+          if self.UniDic in {"gendai","spoken"}:
+            b=s.split("\t")
+            a=b[1].split(",")
+            if len(a)>20:
+              a[9]=a[20]
+            if len(a)>10:
+              a[10]=a[9]
+            for i in range(len(a)):
+              if a[i]=="":
+                a[i]="*"
+            r.append(UniDicMorph(b[0]+"\t"+",".join(a)))
+          else:
+            r.append(UniDicMorph(s))
+        else:
+          a=s.split(",")
+          if a[1]!="B" and a[1]!="I":
+            continue
+          if a[4]=="名詞":
+            p=["名詞","普通名詞","一般","*"]
+          else:
+            p=(a[4]+"-*-*-*-*").split("-")
+          r.append(UniDicMorph(a[2]+"\t"+",".join([p[0],p[1],p[2],p[3],"*","*","*",a[3],a[2],a[5],a[5]])))
+    return r
+
+class UniDicMorph(object):
+  def __init__(self,line):
+    self.morph_line=line.split("\t")
+  def surface(self):
+    return self.morph_line[0]
+  def part_of_speech(self):
+    try:
+      return self.morph_line[1].split(",")[0:6]
+    except:
+      return ["名詞","普通名詞","一般","*","*","*"]
+  def normalized_form(self):
+    try:
+      n=self.morph_line[1].split(",")[7]
+    except:
+      return self.surface()
+    i=n.find("-")
+    if i<1:
+      return n
+    return n[0:i]
+  def reading_form(self):
+    try:
+      return self.morph_line[1].split(",")[10]
+    except:
+      return ""
+
 def load(UniDic=None,UDPipe="japanese-modern"):
   if UniDic==UDPipe:
     UniDic=None
-  return UniDicLanguage(UniDic,UDPipe)
+  if UniDic==None or not UDPipe.startswith("ja_"):
+    return UniDicLanguage(UniDic,UDPipe)
+  import spacy
+  nlp=spacy.load(UDPipe)
+  nlp.tokenizer.tokenizer.tokenize=MeCab2Sudachi(UniDic)
+  return nlp
 
 def to_conllu(item,offset=1):
   if type(item)==Doc:
