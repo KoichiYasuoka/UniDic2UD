615dca0c290d28e0ccc9078af01322f3ed8e2b97
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2020-12-31 15:48:17 +0900

    benchmark script improved

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -12,14 +12,14 @@ import '$MODULE'
 nlp='"$MODULE.$LOAD"'
 with open("'$CONLLU'","r",encoding="utf-8") as f:
   r=f.read()
-for s in r.split("\n"):
-  if s.startswith("# text = "):
-    doc=nlp(s[9:])
-    for t in doc:
-      print("\t".join([str(t.i+1),t.orth_,t.lemma_,t.pos_,t.tag_,"_",str(0 if t.head==t else t.head.i+1),t.dep_.lower(),"_","_" if t.whitespace_ else "SpaceAfter=No"]))
-    print("")
-' > $TMP
-
+with open("'$TMP'","w",encoding="utf-8") as f:
+  for s in r.split("\n"):
+    if s.startswith("# text = "):
+      doc=nlp(s[9:])
+      for t in doc:
+        print("\t".join([str(t.i+1),t.orth_,t.lemma_,t.pos_,t.tag_,"_",str(0 if t.head==t else t.head.i+1),t.dep_.lower(),"_","_" if t.whitespace_ else "SpaceAfter=No"]),file=f)
+      print("",file=f)
+'
 python3 conll18_ud_eval.py $CONLLU $TMP
 rm -f $TMP
 exit 0
